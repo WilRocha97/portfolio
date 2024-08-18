641478e9e5369620f9652eba263f7a14652f28d5
Willian Rodrigues <willrbr1997@gmail.com>
2024-08-18 17:05:06 -0300

    Atualização
    Atualização no botão do tema do site.
    Agora o site aplica o tema de acordo com o navegador.
    Novo fundo dinâmico.
    Adicionado o site Task Watch

--- js/modes.js
+++ js/modes.js
@@ -1,8 +1,33 @@
-const botao = document.getElementById("toggleDarkMode")
 const corpo = document.body
-const fundo = document.getElementById("bg")
+const fundo = document.getElementById("gradient")
+const botao = document.getElementById("toggleDarkMode")
+const imagem = botao.querySelector('img');
 
 botao.addEventListener('click', (e)=> {
     corpo.classList.toggle('light');
-    fundo.classList.toggle('bgLight')
+    fundo.classList.toggle('gradientLight');
+    corpo.classList.toggle('dark');
+    fundo.classList.toggle('gradientDark');
+
+    if (corpo.classList.contains('light')) {
+        // Atualiza o atributo src da imagem
+        imagem.src = 'assets/sun.png';
+        // Atualiza o atributo alt da imagem
+        imagem.alt = 'Modo claro';
+    }
+    else {
+        // Atualiza o atributo src da imagem
+    imagem.src = 'assets/moon.png';
+    // Atualiza o atributo alt da imagem
+    imagem.alt = 'Modo escuro';
+    }
 })
+
+// Verifica se o tema escuro está ativado
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+// Aplica o tema de acordo com a preferência do usuário
+if (!prefersDarkScheme) {
+    corpo.classList.toggle('light');
+    fundo.classList.toggle('bgLight');
+}
